fdeaa922f1d4042754cf75b6f264fd098fb9f1cb
Matthieu Napoli <matthieu@mnapoli.fr>
2015-08-31 14:19:19 +0200

    First commit

---
+++ src/WhoopsMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Stratify\WhoopsModule;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+class WhoopsMiddleware
+{
+    private $whoops;
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        callable $next
+    ) : ResponseInterface
+    {
+        try {
+            return $next($request, $response);
+        } catch (\Exception $e) {
+            if (! $this->whoops) {
+                $this->whoops = new Run();
+                $this->whoops->writeToOutput(false);
+                $this->whoops->allowQuit(false);
+                $this->whoops->pushHandler(new PrettyPageHandler);
+            }
+
+            $output = $this->whoops->handleException($e);
+
+            $response->getBody()->write($output);
+            return $response;
+        }
+    }
+}
